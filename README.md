48b5bac367a91d69b46e63b83d227912fead2936
Janay Bailey <tjanay87@gmail.com>
2019-11-01 21:25:40 +0000

    2nd rd updates

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/Moving`. To experiment with that code, run `bin/console` for an interactive prompt.
 
-TODO: Delete this and the text above, and describe your gem
+TODO: This Gemfile will allow you to use the Penske truck chart and receive a recommendation on a vehicle based on the number of rooms you need to move. 
 
 ## Installation
 
@@ -40,4 +40,4 @@ The gem is available as open source under the terms of the [MIT License](https:/
 
 ## Code of Conduct
 
-Everyone interacting in the Moving project’s codebases, issue trackers, chat rooms and mailing lists is expected to follow the [code of conduct](https://github.com/'techtakeda87'/Moving/blob/master/CODE_OF_CONDUCT.md).
+Everyone interacting in the Moving project’s codebases, issue trackers, chat rooms and mailing lists is expected to follow the [code of conduct](https://github.com/'JoinJanay-JS'/Moving/blob/master/CODE_OF_CONDUCT.md).
--- lib/Moving/cli.rb
+++ lib/Moving/cli.rb
@@ -1,30 +1,54 @@
 class Moving::CLI 
+  attr_accessor :vehicles, :rooms
+  @@all = []
+  
   
   def intro 
     puts "Hello, find your vehicle here!"
-    get_vehicles
-    
+    recommend_vehicle
   end 
-  
-  def vehicle
-end
 
-def room_size()
-  
-end
-  
-  
-  def get_vehicles(room_size)
-    input = nil 
-    while input = gets.strip.downcase
-    case input
-    when room_size = 1
-      puts "Cargo Van"
-    when room_size > 3 != 1
-      puts "16 Foot Truck"
-    when room_size > 7 != 3
-      puts "26 Foot Truck"
+def vehicle_selection
+  puts "Please select '#{vehicle}'"
+    if vehicle == 1
+    then vehicle = 'Cargo Van'
+      elsif vehicle == 2
+      then vehicle = '12 Foot Truck'
+        elsif vehicle == 3
+        then vehicle = '16 Foot Truck'
+          elsif vehicle = 4 || 5
+          then vehicle = '22 Foot Truck'
+            elsif vehicle = 6 || 7
+            then vehicle = '26 Foot Truck'
+            end 
+          end 
+        end
+    
+ 
+
+  def recommend_vehicle
+    x = "#{room}" 
+    if x < 1
+    puts vehicle_selection
+    if x == 2 
+    puts vehicle_selection 
+    if x == 3
+    puts vehicle_select
+  end 
+  end 
+  end 
+
+
+  def initialize(vehicle_select)
+    vehicle_select.each {|v, r| self.send(("#{r}="),v)}
+    @@all << self 
     end 
-  
-end 
+  end
+
+
+
+  def self.all
+  @@all
+  end
+
 
